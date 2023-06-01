18ba996e79b520d92d55386e57949e0c74127f30
intern_diemdt <dtdiem2501@gmail.com>
2023-06-01 15:50:21 +0700

    update: all

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,30 +1,143 @@
-import React from 'react'
-import './index.style.css'
-import Input from 'antd/es/input/Input'
-export default function Register(){
-   return <div className='register-page'>
-      <div className='register-form-container'>
-         <h1 className='title'>Register</h1>
-         <form>
-            <div className ='element'>
-               <label htmlFor='phone' className='form_input'>Phone Number</label>
-               <input type='text' className='form_control' id='phone' name='phone'/>
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import './index.style.css';
+import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
+const initFormValue = {
+  phone: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+const isEmptyValue = (value:string) => {
+  return !value || value.trim().length < 1;
+}
+const isEmailValid = (email:string) => {
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+}
+interface FormValues {
+  [key: string]: string;
+}
+
+export default function Register() {
+  const [formValue, setFormValue] = useState<FormValues>(initFormValue);
+  const [formError, setFormError] = useState({});
+  const [active1, setActive1] = useState(true); 
+  const [active2, setActive2] = useState(true); 
+  const validateForm = () => {
+    const error : { [key: string]: string } = {};
+    if (isEmptyValue(formValue.phone)){
+      error.phone = "Phone is required"; 
+    } else if (formValue.phone.length <10 || formValue.phone.length >=11 || formValue.phone.charAt(0)!=="0"){
+      error.phone = "Phone number is not correct"
+    }
+    if (isEmptyValue(formValue.email)){
+      error.email = "Phone is required"; 
+    }else {
+      if(!isEmailValid(formValue.email)){
+        error.email = "Email is invalid"; 
+      }
+    }
+    if (isEmptyValue(formValue.password)){
+      error.password = "Password is required"; 
+    }
+    if (isEmptyValue(formValue.confirmPassword)){
+      error.confirmPassword = "Confirm password is required"; 
+    }else if (formValue.confirmPassword !== formValue.password){
+      error.confirmPassword = "Confirm password not match"
+    }
+    setFormError(error);
+    return Object.keys(error).length === 0;
+  }; 
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const { value, name } = event.target;
+      setFormValue((prevFormValue) => ({
+
+      ...prevFormValue,
+      [name]: value,
+      }));
+   };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (validateForm()){
+      console.log('Form Value:', formValue);
+    }
+    else {
+      console.log("Form invalid :))")
+    } 
+  };
+  console.log(formError);
+  return (
+    <div className="register-page">
+      <div className="register-form-container">
+        <h1 className="title">Register</h1>
+        <form className="form-parent" onSubmit={handleSubmit}>
+          <div className="form-1">
+            <img className="form-picture" src="./images/pic_register.svg" alt="" />
+          </div>
+          <div className="form-2">
+            <div className="element">
+              <label htmlFor="phone" className="form_input">
+                Phone Number
+              </label>
+              <input
+                  type="number"
+                  className="form_control"
+                  id="phone"
+                  name="phone"
+                  value={formValue.phone}
+                  onChange={handleChange}
+              />
             </div>
-            <div className ='element'>
-               <label htmlFor='email' className='form_input'>Email</label>
-               <input type='text' className='form_control' id='email' name='email'/>
+            <div className="element">
+              <label htmlFor="email" className="form_input">
+                Email
+              </label>
+              <input
+                  type="text"
+                  className="form_control"
+                  id="email"
+                  name="email"
+                  value={formValue.email}
+                  onChange={handleChange}
+              />
             </div>
-            <div className ='element'>
-               <label htmlFor='password' className='form_input'>PassWord</label>
-               <input type='text' className='form_control' id='password' name='password'/>
+            <div className="element">
+              <label htmlFor="password" className="form_input">
+                Password
+              </label>
+              <input
+                  type={`${active1 ? "password":"text"}`} // điều kiện active 
+                  // type='password'
+                  className="form_control"
+                  id="password"
+                  name="password"
+                  value={formValue.password}
+                  onChange={handleChange}
+              />
+              {active1 ? <EyeOutlined className="icon" onClick={()=>setActive1(!active1)} /> : <EyeInvisibleOutlined className="icon" onClick={()=>setActive1(!active1)} />}
+              {/* <EyeInvisibleOutlined className="icon" />  */}
+               
             </div>
-            <div className ='element'>
-               <label htmlFor='confirm_password' className='form_input'>Confirm PassWord</label>
-               <input type='text' className='form_control' id='confirm_password' name='confirmPassword'/>
+            <div className="element">
+              <label htmlFor="confirmPassword" className="form_input">
+                Confirm Password
+              </label>
+              <input
+                  type={`${active2 ? "password":"text"}`}
+                  className="form_control"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  value={formValue.confirmPassword}
+                  onChange={handleChange}
+              />
+              {active2 ? <EyeOutlined className="icon" onClick={()=>setActive2(!active2)} /> : <EyeInvisibleOutlined className="icon" onClick={()=>setActive2(!active2)} />}
+              {/* <EyeInvisibleOutlined className="icon" /> */}
             </div>
 
-            <input type='submit' className ='submit-btn' value='Register'/>
-         </form> 
+            <input type="submit" className="submit-btn" value="Register" />
+          </div>
+        </form>
       </div>
-   </div>
+    </div>
+  );
 }
