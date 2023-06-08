477835c96e91364bbffd4efd17affc3812b9ddc9
intern_diemdt <dtdiem2501@gmail.com>
2023-06-08 09:38:31 +0700

    update: feature/comment

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import './index.style.scss';
 import { Button, Modal } from "antd";
+import { IMAGE_PATH } from "src/constants/images";
+import { UserOutlined } from "@ant-design/icons";
 
 export default function Comment() {
     const showModal = () => {
@@ -17,14 +19,23 @@ export default function Comment() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     return (
         <div>
-            
-            <Button type="primary" onClick={showModal} className={"fixed bottom-28 right-4"}>
-                Comment
-            </Button>
-            <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+            <img src={IMAGE_PATH.COMMENT} onClick={showModal} className={"fixed bottom-28 right-4"}></img>
+            <Modal open={isModalOpen} width={"300px"} footer={null} closable={false}>
+                <div className=" ">
+                    <div className=" ">
+                        <div className="flex items-center mb-2">
+                            <UserOutlined />
+                            <h4 className="m-0 ml-2">Hong Ha Le</h4>
+                        </div>
+                        <textarea className="w-full h-10 mb-2 border-solid border-gray-200 hover:border-solid hover:border-gray-200">
+                            Leave a comment...
+                        </textarea>
+                        <div className="flex justify-end">
+                            <input type="submit" className="h-8 w-16 rounded-md border-white font-bold hover:bg-gray-300 hover:text-gray-50" value="Cancel" />
+                            <input type="submit" className="ml-2 h-8 w-16 rounded-md border-white font-bold hover:bg-gray-300 hover:text-gray-50" value="Post" />
+                        </div>
+                    </div>
+                </div>
             </Modal>
         </div>
     );
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -31,4 +31,5 @@ export const IMAGE_PATH = {
   TV: `${PUBLIC_URL}/images/resource/TV.png`,
   VECTOR: `${PUBLIC_URL}/images/resource/vector.svg`,
   IMAGE: `${PUBLIC_URL}/images/resource/image1.svg`,
+  COMMENT: `${PUBLIC_URL}/images/comment.svg`
 };
