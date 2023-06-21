c88b74185269ab65a00bd73db2fe4cdda0f86e66
intern_diemdt <dtdiem2501@gmail.com>
2023-06-21 14:12:22 +0700

    update: form

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,63 +1,86 @@
-import React, { ChangeEvent, useState } from "react";
+import React from "react";
 import './index.style.scss';
-import { UserOutlined } from "@ant-design/icons";
 
-export default function Comment() {
-    // const showModal = () => {
-    //     setIsModalOpen(true);
-    // };
-
-    // const handleOk = () => {
-    //     setIsModalOpen(false);
-    // };
-    const handleCancel = () => {
-        setValue('');
-    };
-    const handlePost = () => {
-        setValue('');
-        setComment((prevComments) => [...prevComments, value]);
-    };
-    const [comment, setComment] = useState<string[]>([]);
-    const [value, setValue] = useState('');
-    const handleOnchange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-        setValue(event.target.value);
-    };
-    const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+export default function Comment(){
     return (
-
-        <div>
-            <div className=" ">
-                <div className=" ">
-                    <div className="flex items-center mb-2">
-                        <UserOutlined />
-                        <h4 className="m-0 ml-2">Hong Ha Le</h4>
-                    </div>
-                    <textarea value={value} onChange={handleOnchange} rows={value.split('\n').length} placeholder="Leave a comment..." className="w-full h-10 mb-2 border-solid border-gray-200 focus:border-solid focus:border-gray-400 focus:outline-none resize-none overflow-hidden">
-                    </textarea>
-                    <div className="flex justify-end">
-                        <input type="submit" className="h-8 w-16 rounded-md border-white font-bold active:bg-gray-300 active:text-gray-50 cursor-pointer" value="Cancel" onClick={handleCancel} />
-                        <input type="submit" className={`ml-2 h-8 w-16 rounded-md border-white font-bold ${value === '' ? "cursor-no-drop" :"active:bg-gray-300 active:text-gray-50 cursor-pointer"}`} value="Post" disabled={value===''?true:false} onClick={handlePost} />
-                    </div>
-                </div>
-                <hr />
-                <div className="max-h-80 pr-1 custom-scroll">
-                    {comment.map((val, index) => {
-                        return (
-                            <div
-                                key={index}
-                                className="border-gray-300 border-solid rounded-lg mb-3 p-2"
-                            >
-                                <div className="flex items-center mb-2">
-                                    <UserOutlined />
-                                    <h4 className="m-0 ml-2">Hong Ha Le on {date}</h4>
-                                </div>
-                                <p className="m-0 ">{val}</p>
+        <>
+            <div className="container mt-5 mb-5">
+                <div className="flex justify-center items-center">
+                    <div className="">
+                        <div className="card">
+                            <div className="p-3">
+                                <h6>Comments</h6>
                             </div>
-                        );
-                    })}
+                            <div className="mt-3 flex flex-row items-center p-3 form-color">
+                                <img src="https://i.imgur.com/zQZSWrt.jpg" width="50" className="rounded-circle mr-2" />
+                                <input type="text" className="h-12 w-full rounded-3xl border-solid border focus:!bg-gray-200  focus:!outline-none focus:!border-solid focus:!border-red-300" placeholder="Enter your comment..." />
+                            </div>
+                            <div className="mt-2">
+                                <div className="flex flex-row p-3">
+                                    <img src="https://i.imgur.com/zQZSWrt.jpg" width="40" height="40" className="rounded-circle mr-3" />
+                                    <div className="w-100">
+                                        <div className="flex justify-between items-center">
+                                            <div className="flex flex-row items-center">
+                                                <span className="mr-2">Brian selter</span>
+                                                <small className="c-badge">Top Comment</small>
+                                            </div>
+                                            <small>12h ago</small>
+                                        </div>
+                                        <p className="text-justify comment-text mb-0">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                                        </p>
+                                        <div className="flex flex-row user-feed">
+                                            <span className="wish"><i className="fa fa-heartbeat mr-2"></i>24</span>
+                                            <span className="ml-3"><i className="fa fa-comments-o mr-2"></i>Reply</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex flex-row p-3">
+                                    <img src="https://i.imgur.com/3J8lTLm.jpg" width="40" height="40" className="rounded-circle mr-3" />
+                                    <div className="w-100">
+                                        <div className="flex justify-between items-center">
+                                            <div className="flex flex-row items-center">
+                                                <span className="mr-2">Seltos Majito</span>
+                                                <small className="c-badge">Top Comment</small>
+                                            </div>
+                                            <small>2h ago</small>
+                                        </div>
+                                        <p className="text-justify comment-text mb-0">
+                                            Tellus in hac habitasse platea dictumst vestibulum. Lectus nulla at volutpat diam ut venenatis tellus. Aliquam etiam erat velit scelerisque in dictum non consectetur. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec. Tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra. Aliquam faucibus purus in massa.
+                                        </p>
+                                        <div className="flex flex-row user-feed">
+                                            <span className="wish"><i className="fa fa-heartbeat mr-2"></i>14</span>
+                                            <span className="ml-3"><i className="fa fa-comments-o mr-2"></i>Reply</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex flex-row p-3">
+                                    <img src="https://i.imgur.com/agRGhBc.jpg" width="40" height="40" className="rounded-circle mr-3" />
+                                    <div className="w-100">
+                                        <div className="flex justify-between items-center">
+                                            <div className="flex flex-row items-center">
+                                                <span className="mr-2">Maria Santola</span>
+                                                <small className="c-badge">Top Comment</small>
+                                            </div>
+                                            <small>12h ago</small>
+                                        </div>
+                                        <p className="text-justify comment-text mb-0">
+                                            Id eu nisl nunc mi ipsum faucibus. Massa massa ultricies mi quis hendrerit dolor. Arcu bibendum at varius vel pharetra vel turpis nunc eget. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Urna condimentum mattis pellentesque id.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                                        </p>
+                                        <div className="flex flex-row user-feed">
+                                            <span className="wish"><i className="fa fa-heartbeat mr-2"></i>54</span>
+                                            <span className="ml-3"><i className="fa fa-comments-o mr-2"></i>Reply</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div >
-    );
-}
+            </div> 
+        </>
+            )
+};
+
+
+
