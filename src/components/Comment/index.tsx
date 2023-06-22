c80f6de2e581f0c90f6f2a3a6806f1a9a3f9b0da
intern_diemdt <dtdiem2501@gmail.com>
2023-06-22 21:30:25 +0700

    update: comment

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,29 +1,58 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import './index.style.scss';
-
+import { HeartFilled } from "@ant-design/icons";
+interface FormValue {
+    img: string;
+    name: string;
+    content: string
+}
 export default function Comment() {
+    const [reply, setReply] = useState(false);
+    const [value, setValue] = useState("");
+    // const [listComment,setListComment]=useState([])
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+        console.log(value);
+
+    };
+    const initFormValue: FormValue = {
+        img: '',
+        name: '',
+        content: ''
+    };
+    const [myArray, setMyArray] = useState<FormValue[]>([]);
+
+    const addSubArray = () => {
+        setMyArray((prevArray) => [...prevArray, initFormValue]);
+    };
+    
     return (
         <>
             <div className="container mt-5 mb-5">
                 <div className="flex justify-center items-center ">
                     <div className="">
-                        <div className="border-none bg-[#fff] ml-auto">
+                        <div className="border-none ml-auto">
                             <div className="p-2 text-lg">
                                 <h6>Comments</h6>
                             </div>
-                            <div className="mt-2 flex items-center p-3 bg-[#fafafa]">
-                                <img src="https://i.imgur.com/zQZSWrt.jpg" width="44" className="rounded-full mr-2" />
-                                <input
-                                    type="text"
-                                    className="h-12 w-full rounded-3xl border-solid border focus:!bg-gray-50  focus:!outline-none focus:!border-solid focus:!border-gray-300 focus:border-2"
-                                    placeholder="Enter your comment..."
-                                />
-                            </div>
-                            <div className="flex justify-end">
-                                <button type="button" className="font-bold text-sm h-8 w-20 rounded-full mr-8 border-none bg-white hover:bg-gray-200">Cancel</button>
-                                <button type="button" className="font-bold text-sm h-8 w-24 rounded-full mr-8 border-none bg-white hover:bg-gray-200">Comment</button> 
+                            <div className="mt-2 flex flex-col  p-3 bg-white">
+                                <div className="flex items-center ">
+                                    <img src="https://i.imgur.com/zQZSWrt.jpg" width="44" className="rounded-full mr-2" />
+                                    <input
+                                        type="text"
+                                        className="px-5 h-12 w-full rounded-3xl border-solid border focus:!bg-gray-50  focus:!outline-none focus:!border-solid focus:!border-gray-300 focus:border-2"
+                                        placeholder="Enter your comment..."
+                                        value={value}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className=" mt-3 flex justify-end">
+                                    <button type="button" className="font-bold text-sm h-9 w-24 rounded-full mr-8 border-none bg-white hover:bg-gray-200 cursor-pointer">Cancel</button>
+                                    <button type="button" onClick={() => { setValue("") }} disabled={value.length === 0 ? true : false} className="font-bold text-sm h-9 w-28 rounded-full mr-8 border-none bg-white hover:bg-gray-200 cursor-pointer">Comment</button>
+                                </div>
                             </div>
                             <div className="mt-2">
+
                                 <div className="flex flex-row p-3">
                                     <img src="https://i.imgur.com/zQZSWrt.jpg" width="38" height="38" className="rounded-full mr-3" />
                                     <div className="w-full">
@@ -37,12 +66,29 @@ export default function Comment() {
                                         <p className="text-justify text-sm mb-0">
                                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
                                         </p>
-                                        <div className="flex flex-row text-sm mt-3">
-                                            <span className="text-[#35b69f]"><i className="fa fa-heartbeat mr-2"></i>24</span>
-                                            <span className="ml-3"><i className="fa fa-comments-o mr-2"></i>Reply</span>
+                                        <div className="flex text-sm mt-3 text-center">
+                                            <span className="text-[#35b69f]">24</span>
+                                            <HeartFilled className="mt-1 ml-3 text-red-600 hover:cursor-pointer " />
+                                            <span onClick={() => setReply(true)} className="ml-8 hover:cursor-pointer h-7 w-16 rounded-full hover:bg-gray-200 flex justify-center items-center">Reply</span>
                                         </div>
                                     </div>
                                 </div>
+                                {
+                                    reply ? (<div className="mt-2 flex flex-col  p-3 bg-white">
+                                        <div className="flex items-center ">
+                                            <img src="https://i.imgur.com/zQZSWrt.jpg" width="44" className="rounded-full mr-2" />
+                                            <input
+                                                type="text"
+                                                className="h-12 w-full rounded-3xl border-solid border focus:!bg-gray-50  focus:!outline-none focus:!border-solid focus:!border-gray-300 focus:border-2"
+                                                placeholder="Enter your comment..."
+                                            />
+                                        </div>
+                                        <div className=" mt-3 flex justify-end">
+                                            <button type="button" className="font-bold text-sm h-9 w-24 rounded-full mr-8 border-none bg-white hover:bg-gray-200 cursor-pointer">Cancel</button>
+                                            <button type="button" className="font-bold text-sm h-9 w-28 rounded-full mr-8 border-none bg-white hover:bg-gray-200 cursor-pointer">Comment</button>
+                                        </div>
+                                    </div>) : ""
+                                }
                                 <div className="flex flex-row p-3">
                                     <img src="https://i.imgur.com/3J8lTLm.jpg" width="38" height="38" className="rounded-full mr-3" />
                                     <div className="w-100">
@@ -73,7 +119,7 @@ export default function Comment() {
                                             <small className="">12h ago</small>
                                         </div>
                                         <p className="text-justify text-sm mb-0">
-                                            Id eu nisl nunc mi ipsum faucibus. Massa massa ultricies mi quis hendrerit dolor. Arcu bibendum at varius vel pharetra vel turpis nunc eget. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Urna condimentum mattis pellentesque id.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                                            Id eu nisl nunc mi ipsum faucibus. Massa massa ultricies mi quis hendrerit dolor. Arcu bibendum at varius vel pharetra vel turpis nunc eget. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Urna condimentum mattis pellentesque id.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.Habitasse platea dictumst quisque sagittis purus sit amet volutpat.Habitasse platea dictumst quisque sagittis purus sit amet volutpat.Habitasse platea dictumst quisque sagittis purus sit amet volutpat.Habitasse platea dictumst quisque sagittis purus sit amet volutpat.Habitasse platea dictumst quisque sagittis purus sit amet volutpat
                                         </p>
                                         <div className="flex flex-row text-sm mt-3">
                                             <span className="text-[#35b69f]"><i className="fa fa-heartbeat mr-2"></i>54</span>
