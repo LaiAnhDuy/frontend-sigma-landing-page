3074f67c1daac6ad795463415f371568a0564068
intern_diemdt <dtdiem2501@gmail.com>
2023-06-22 09:33:28 +0700

    update: featue/form

--- src/pages/Login/ForgotPassword/index.tsx
+++ src/pages/Login/ForgotPassword/index.tsx
@@ -3,6 +3,7 @@ import React, { ChangeEvent, FormEvent, useState } from 'react';
 import './index.style.scss';
 import { isEmailValid, isEmptyValue } from 'src/utils';
 import { IMAGE_PATH } from 'src/constants/images';
+import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
 const initFormValue = {
     email: "",
     newPassword: "",
@@ -15,6 +16,8 @@ export default function ForgotPassword() {
     const [formValue, setFormValue] = useState<FormValues>(initFormValue);
     const [formError, setFormError] = useState<FormValues>({});
     const [selection, setSelect] = useState(1);
+    const [activePassword, setActivePassword] = useState(true);
+    const [activeConfirmPassword, setActiveConfirmPassword] = useState(true);
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         const { value, name } = event.target;
         setFormValue((prevFormValue) => ({
@@ -98,7 +101,7 @@ export default function ForgotPassword() {
                                     <h3 className="text-white text-xl">Reset your password</h3>
                                     <form onSubmit={handleSubmitPassword} className="text-center">
                                         <input
-                                            type="text"
+                                            type={`${activePassword ? "password" : "text"}`}
                                             id="newPassword"
                                             name="newPassword"
                                             value={formValue.newPassword}
@@ -106,9 +109,10 @@ export default function ForgotPassword() {
                                             placeholder="New password"
                                             className={`w-[280px] h-10 border-solid rounded-sm mt-2 focus:outline-none focus:border-white ${formError.newPassword ? "border-red-400" : "border-white"}`}
                                         />
+                                        {activePassword ? <EyeOutlined className="-ml-[30px]" onClick={() => setActivePassword(!activePassword)} /> : <EyeInvisibleOutlined className="-ml-[30px]" onClick={() => setActivePassword(!activePassword)} />}
                                         <div className="h-5 -ml-28 font-semibold text-sm text-red-700 mt-2">{formError.newPassword}</div>
                                         <input
-                                            type="text"
+                                            type={`${activeConfirmPassword ? "password" : "text"}`}
                                             id="confirmPassword"
                                             name="confirmPassword"
                                             value={formValue.confirmPassword}
@@ -116,6 +120,7 @@ export default function ForgotPassword() {
                                             placeholder="Confirm password"
                                             className={`w-[280px] h-10 border-solid rounded-sm mt-2 focus:outline-none focus:border-white ${formError.confirmPassword ? "border-red-400" : "border-white"}`}
                                         />
+                                        {activeConfirmPassword ? <EyeOutlined className="-ml-[30px]" onClick={() => setActiveConfirmPassword(!activeConfirmPassword)} /> : <EyeInvisibleOutlined className="-ml-[30px]" onClick={() => setActiveConfirmPassword(!activeConfirmPassword)} />}
                                         <div className="h-5 -ml-24 font-semibold text-sm text-red-700 mt-2">{formError.confirmPassword}</div>
                                         <input type="submit" value="Reset password" className="mt-10 mb-20 w-[280px] h-10 rounded-full border-solid border-white bg-white text-base text-green-700 font-bold cursor-pointer active:bg-slate-300 active:border-none" />
                                     </form>
